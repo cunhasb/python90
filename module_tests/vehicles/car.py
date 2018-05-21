1f981e899b0ddd306aa601801e01f5129b167e9f
Fabiano Cunha <cunhasb@aol.com>
2018-05-21 14:15:48 -0400

    default values

--- module_tests/vehicles/car.py
+++ module_tests/vehicles/car.py
@@ -8,7 +8,7 @@ class Car:
     def open_door(self, door_number):
         print ("Opening door: %s" % str(door_number))
 
-    def blinker_control(self, left_blinker, right_blinker):
+    def blinker_control(self, left_blinker="Off", right_blinker="Off"):
         self.left_blinker = left_blinker
         self.right_blinker = right_blinker
         print ("L: %s, R: %s" % (self.left_blinker, self.right_blinker))
