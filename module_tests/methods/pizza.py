16dbb645b4abb77824f5461180c1bb18072b546e
Fabiano Cunha <cunhasb@aol.com>
2018-05-21 23:18:03 -0400

    pizza.py

---
+++ module_tests/methods/pizza.py
@@ -0,0 +1,14 @@
+class Pizza:
+    def __init__(self, ingredients):
+        self.ingredients = ingredients
+
+    def __repr__(self):
+        return f'Pizza({self.ingredients})'
+
+    @classmethod
+    def margherita(cls):
+        return cls(['cheese', 'tomatoes'])
+
+    @classmethod
+    def prosciutto(cls):
+        return cls(['cheese', 'tomatoes', 'ham', 'mushrooms'])
